e00a1fecf791e1a9b0a7da7ea3b2e4d3622743c7
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-10-10 15:22:28 +0700

    mendeklarasikan function ConditionalStatement

--- app/src/main/java/com/example/questbasickotlin_0060/ConditionalExpression.kt
+++ app/src/main/java/com/example/questbasickotlin_0060/ConditionalExpression.kt
@@ -3,3 +3,8 @@ package com.example.questbasickotlin_0060
 //conditional expression
 //conditional expression adalah ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
 //conditional expression menggunakan if-else
+
+fun ConditionalStatement(){
+    println("=== Conditional Expression ===")
+
+}
