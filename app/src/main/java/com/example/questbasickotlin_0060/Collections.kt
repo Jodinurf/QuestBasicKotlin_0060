fb8e1371c56b4abc65ab4fc281f2a64c33fae228
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-10-10 14:12:54 +0700

    membuat variable read-only yaitu ReadOnlyAbjad yang berisikan function listOf untuk membuat list Read-Only

--- app/src/main/java/com/example/questbasickotlin_0060/Collections.kt
+++ app/src/main/java/com/example/questbasickotlin_0060/Collections.kt
@@ -1,2 +1,8 @@
 package com.example.questbasickotlin_0060
 
+fun ContohList() {
+    println("=== List ===")
+    //List read-only
+    val readOnlyAbjad = listOf("A", "B", "C")
+    println(readOnlyAbjad)
+}
