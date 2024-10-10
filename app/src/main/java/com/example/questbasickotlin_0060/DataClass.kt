8dfee532494ca2701d9b4d0370874e99532fe2b3
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-10-10 15:56:01 +0700

    materi DataClass.kt

--- app/src/main/java/com/example/questbasickotlin_0060/DataClass.kt
+++ app/src/main/java/com/example/questbasickotlin_0060/DataClass.kt
@@ -1,2 +1,8 @@
 package com.example.questbasickotlin_0060
 
+import android.provider.ContactsContract.CommonDataKinds.Email
+
+//data class
+// data class adalah class yang digunakan untuk menyimpan data.
+// data class menyediakan fungsi untuk meng-override (menumpuk) fungsi equals(), hashCode(), dan toString().
+// Data class juga menyediakan fungsi copy() untuk membuat salinan objek dengan beberapa perubahan.
