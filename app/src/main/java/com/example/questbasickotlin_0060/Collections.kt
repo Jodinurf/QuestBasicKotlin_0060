26e37745750c48bc3ac479593635acb1d3970515
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-10-10 14:33:07 +0700

    Materi tentang set

--- app/src/main/java/com/example/questbasickotlin_0060/Collections.kt
+++ app/src/main/java/com/example/questbasickotlin_0060/Collections.kt
@@ -31,3 +31,9 @@ fun ContohList() {
     val shapesLocked: List<String> = shape
     println(shapesLocked)
 }
+
+//Set adalah kumpulan data yang tidak memiliki posisi atau urutan tertentu hanya menyimpan data unique(Tidak boleh ada duplikasi)
+//Set bersifat unordered, artinya data yang dimasukkan tidak memiliki posisi tertentu
+//set bersifat mutable dan read-only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+//set mutable menggunakan mutableSetOf()
+//set read-only menggunakan setOf()
