14f865945f11e79d950f969487325dd8c4146e7b
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-10-10 16:17:32 +0700

    materi materi function yang dibuat tanpa parameter, dengan parameter, dengan argumen yang telah diberikan nama, default parameter

--- app/src/main/java/com/example/questbasickotlin_0060/Functions.kt
+++ app/src/main/java/com/example/questbasickotlin_0060/Functions.kt
@@ -1,2 +1,39 @@
 package com.example.questbasickotlin_0060
 
+fun withoutParameter(){
+    println("=== Without Parameter ===")
+    println("Hellow, World")
+}
+
+fun withParameter(name:String){
+    println()
+    println("=== With Parameter ===")
+    println("Hello, $name")
+}
+
+//named argument
+//named argument merupakan cara untuk memanggil fungsi dengan menyebutkan nama parameter
+fun withNamedArgument(name: String, age: Int){
+    println()
+    println("=== With Named Argument ===")
+    println("Hello, $name! You are $age years old")
+}
+
+//default parameter value
+//default parameter value merupakan cara untuk memberikan nilai default pada parameter fungsi
+fun withDefaultParameter(name: String = "Indra", age: Int){
+    println()
+    println("=== With Default Parameter ===")
+    println("Hello, $name you are $age years old")
+}
+
+fun withReturn(panjang : Int, lebar : Int):Int{
+    return println(panjang * lebar)
+}
+
+fun  main() {
+    withoutParameter()
+    withParameter("John")
+    withNamedArgument("John", age = 30)
+    withDefaultParameter(age = 25)
+}
