4ca8663ad1d5c89fefe272abcaf43a918f5ac354
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-10-10 16:43:15 +0700

    membuat variable nullable dan mengechek variabel neverNull dan menampilkannya

--- app/src/main/java/com/example/questbasickotlin_0060/NullSafety.kt
+++ app/src/main/java/com/example/questbasickotlin_0060/NullSafety.kt
@@ -3,5 +3,31 @@ package com.example.questbasickotlin_0060
 fun nullSafety(){
     // neverNull mempunyai type string
     var neverNull: String = "This can't be null"
+
+    // throws a compiler error
+    // neverNull = null
+
+    //nullable mempunyai nullable tipe string
+    var nullable: String? = "You can keep a null here"
+    //this is OK
+    nullable = null;
+
+    //check for null in conditions
+    if (neverNull == null){
+        println("inferredNonNull is null")
+    } else {
+        println("inferredNonNull is not null")
+    }
+
+    // safe call operator
+    println(neverNull.length) // 18
+    println(nullable?.length) // null
+
+    //notNull doesn't accept null values
+    fun strLength(notNull: String):Int{
+        return notNull.length
+    }
+
+    println(strLength(neverNull))//18
 }
 
