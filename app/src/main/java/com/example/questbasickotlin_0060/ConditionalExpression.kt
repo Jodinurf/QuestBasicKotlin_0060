09e08aec0d3b27bf68c419959fececfa61d93842
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-10-10 15:19:59 +0700

    penjelasan materi ConditionalExpression.kt

--- app/src/main/java/com/example/questbasickotlin_0060/ConditionalExpression.kt
+++ app/src/main/java/com/example/questbasickotlin_0060/ConditionalExpression.kt
@@ -1,2 +1,5 @@
 package com.example.questbasickotlin_0060
 
+//conditional expression
+//conditional expression adalah ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
+//conditional expression menggunakan if-else
