fcb5093222c80e40dda5fd3ea9c415ba4907ce6f
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-10-10 16:03:46 +0700

    penggunaan dataclass di masing masing variable data mulai dari fungsi equals, copy dan copy dengan perubahan

--- app/src/main/java/com/example/questbasickotlin_0060/DataClass.kt
+++ app/src/main/java/com/example/questbasickotlin_0060/DataClass.kt
@@ -11,3 +11,20 @@ data class DataClass(
     val id:Int,
     var email: String
 )
+
+fun main(){
+    val data = DataClass(1, "indra@gmai.com")
+    println(data)
+
+    //fungsi Equals
+    val data2 = DataClass(1, "makan@gmail.com")
+    println(data == data2)
+
+    //fungsi copy
+    val data3 = data.copy()
+    println(data3)
+
+    //fungsi copy dengan perubahan
+    val data4 = data.copy(email = "sahar@gmail.com")
+    println(data4)
+}
