2b7b9a681eaa51f8d54790337d4a40799bb66952
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-10-10 16:22:02 +0700

    materi LambdaFunction.kt

--- app/src/main/java/com/example/questbasickotlin_0060/LambdaFunction.kt
+++ app/src/main/java/com/example/questbasickotlin_0060/LambdaFunction.kt
@@ -1,2 +1,6 @@
 package com.example.questbasickotlin_0060
 
+// Lambda functions
+// Lambda Functions adalah fungsi yang tidak memiliki nama
+// Lambda Function biasanya digunakan untuk membuat fungsi yang sederhana
+// lambda function menggunakan tanda panah (->) untuk memisahkan parameter dan body fungsi
